7bcaf926cc1b85ea56f1411473691a00339ef0dc
SilverizedVGC <pikacuber@gmail.com>
2026-04-21 16:40:44 -0700

    Start on main()

--- build_tree.cpp
+++ build_tree.cpp
@@ -7,7 +7,7 @@ using namespace std;
 // Build syntax tree from tokens.
 // Operator precedence: PEMDAS (Parentheses, Exponents, Multiplication/Division, Addition/Subtraction)
 
-void build_tree(const vector<Token>& tokens) {
+void build_tree(const vector<string>& tokens) {
     // This function will take the list of tokens and build a syntax tree.
     while (!tokens.empty()) {
         
--- main.cpp
+++ main.cpp
@@ -3,8 +3,30 @@
 #include <vector>
 #include <cstdlib>
 #include <unistd.h>
+#include <fstream>
 using namespace std;
 
 int main(void) {
+    // File input
+    string filename;
+    cout << "Enter the filename of the Python code: ";
+    cin >> filename;
+    ifstream file(filename);
+    if (!file.is_open()) {
+        cerr << "Error opening file: " << filename << endl;
+        return 1;
+    }
+    // Read file line by line and pass into lexical analyzer
+    string line;
+    while (getline(file, line)) {
+        lexical_analyzer(line);
+    }
+    // Note: Pass the tokens from the lexical analyzer to the syntax tree builder
+    vector<string> tokens; // This should be filled with tokens from the lexical analyzer
+    build_tree(tokens);
+
+
+
+
     return 0;
 }
